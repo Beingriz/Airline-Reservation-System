2060c8a04e9ef482e98342392749a014f411b79f
Md Rizwan <68804148+Beingriz@users.noreply.github.com>
2022-12-24 14:02:08 +0530

    Design of Airline Management Admin Module is Completed

---
+++ AirlineManagementSystem/src/main/java/com/mycompany/airlinemanagementsystem/AirlineReservation.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.airlinemanagementsystem;
+
+/**
+ *
+ * @author mdriz
+ */
+public class AirlineReservation {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
